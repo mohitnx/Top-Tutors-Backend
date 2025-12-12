2decbf3242d4b7cdd4ede209a54c5c4181842f48
mohitnx <mohitneupane157@gmail.com>
2025-12-12 14:33:53 +0545

    Cleanup: Remove trailing whitespace in multiple files

--- src/modules/auth/guards/roles.guard.ts
+++ src/modules/auth/guards/roles.guard.ts
@@ -26,3 +26,4 @@ export class RolesGuard implements CanActivate {
   }
 }
 
+
--- src/modules/messages/index.ts
+++ src/modules/messages/index.ts
@@ -2,3 +2,4 @@ export * from './messages.module';
 export * from './messages.service';
 export * from './messages.controller';
 
+
